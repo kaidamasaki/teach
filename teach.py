9d39c596a090674bb442390f77db7dc5fc9ec91f
Kaida Masaki <kaida@adadevelopersacademy.org>
2021-03-10 13:53:18 -0800

    Use app.secret_key

--- teach.py
+++ teach.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 from flask_meld import Meld
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.secret_key = os.getenv('SECRET_KEY')
 
 meld = Meld()
 meld.init_app(app)
